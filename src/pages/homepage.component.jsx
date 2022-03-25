a5143e8238c3c768543754a374238c7bfbb0c15b
acxe21 <jeromedacasin21@gmail.com>
2022-03-26 01:28:11 +0800

    broken our homepage to smaller component and maek a hover

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,13 @@
+import './homepage.style.scss'
+import DirectoryMenu from '../components/directory/directory-menu.component'
+
+const HomePage = () => (
+    <div className="homepage">
+
+        <DirectoryMenu/>
+
+    </div>
+)
+
+
+export default HomePage
